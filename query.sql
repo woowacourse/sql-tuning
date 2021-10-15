a8d5284611edaa4d4265317605bb3f4a0301c661
Kimun Kim <kkm97351@gmail.com>
2021-10-15 10:05:33 +0900

    feat: 요구사항 B, Coding As A Boddy 문제 풀이

--- query.sql
+++ query.sql
@@ -1,3 +1,5 @@
+-- A
+
 -- 1번 쿼리
 SELECT 상위사원.사원번호, 상위사원.이름, 상위사원.직급명, 상위사원.연봉, 사원출입기록.지역, 사원출입기록.입출입구분 FROM
 (SELECT 사원.사원번호, 사원.이름, 직급.직급명, 급여.연봉 FROM tuning.직급
@@ -13,3 +15,17 @@ order by 상위사원.연봉 desc;
 -- 2번 인덱스 추가를 통한 성능 향상
 CREATE INDEX idx_직급명 on 직급 (직급명);
 CREATE INDEX idx_사원번호 on 사원출입기록 (사원번호);
+
+
+-- B
+-- Coding As A Hobby
+
+CREATE INDEX `idx_programmer_hobby`  ON `subway`.`programmer` (hobby) COMMENT '' ALGORITHM DEFAULT LOCK DEFAULT
+-- 인덱스 생성
+
+select
+	(sum(case when stat.hobby = 'yes' then stat.count end) / sum(stat.count)) * 100 as 'yes(%)',
+    (sum(case when stat.hobby = 'no' then stat.count end) / sum(stat.count)) * 100 as 'no(%)'
+from 
+	(SELECT hobby, COUNT(hobby) as count FROM subway.programmer group by hobby) as stat
+-- 0.044 sec

118872646310a2f889c3c710fba51405e65da07a
Kimun Kim <kkm97351@gmail.com>
2021-10-15 10:19:17 +0900

    style: 들여쓰기

--- query.sql
+++ query.sql
@@ -1,15 +1,16 @@
 -- A
 
 -- 1번 쿼리
-SELECT 상위사원.사원번호, 상위사원.이름, 상위사원.직급명, 상위사원.연봉, 사원출입기록.지역, 사원출입기록.입출입구분 FROM
-(SELECT 사원.사원번호, 사원.이름, 직급.직급명, 급여.연봉 FROM tuning.직급
-JOIN 사원 ON 사원.사원번호 = 직급.사원번호
-JOIN 급여 ON 급여.사원번호 = 사원.사원번호
-WHERE 직급.직급명='Manager' AND 직급.종료일자='9999-01-01' AND 급여.종료일자='9999-01-01'
-ORDER BY 급여.연봉 DESC
-LIMIT 5) AS 상위사원
-JOIN 사원출입기록 ON 사원출입기록.사원번호=상위사원.사원번호
-WHERE 입출입구분='O'
+SELECT 상위사원.사원번호, 상위사원.이름, 상위사원.직급명, 상위사원.연봉, 사원출입기록.지역, 사원출입기록.입출입구분
+  FROM
+    (SELECT 사원.사원번호, 사원.이름, 직급.직급명, 급여.연봉
+		  FROM tuning.직급
+        JOIN 사원 ON 사원.사원번호 = 직급.사원번호
+        JOIN 급여 ON 급여.사원번호 = 사원.사원번호
+      WHERE 직급.직급명='Manager' AND 직급.종료일자='9999-01-01' AND 급여.종료일자='9999-01-01'
+      ORDER BY 급여.연봉 DESC LIMIT 5) AS 상위사원
+    JOIN 사원출입기록 ON 사원출입기록.사원번호=상위사원.사원번호
+    WHERE 입출입구분='O'
 ORDER BY 상위사원.연봉 DESC;
 
 -- 2번 인덱스 추가를 통한 성능 향상
@@ -24,8 +25,8 @@ CREATE INDEX `idx_programmer_hobby` ON `subway`.`programmer` (hobby) COMMENT '' 
 -- 인덱스 생성
 
 SELECT
-	(sum(CASE WHEN stat.hobby = 'yes' THEN stat.count END) / sum(stat.count)) * 100 AS 'yes(%)',
-    (sum(CASE WHEN stat.hobby = 'no' THEN stat.count END) / sum(stat.count)) * 100 AS 'no(%)'
+  (sum(CASE WHEN stat.hobby = 'yes' THEN stat.count END) / sum(stat.count)) * 100 AS 'yes(%)',
+  (sum(CASE WHEN stat.hobby = 'no' THEN stat.count END) / sum(stat.count)) * 100 AS 'no(%)'
 FROM
 	(SELECT hobby, COUNT(hobby) AS count FROM subway.programmer GROUP BY hobby) AS stat
 -- 0.044 sec
@@ -39,7 +40,8 @@ ALTER TABLE covid ADD FOREIGN KEY(programmer_id) REFERENCES programmer(id);
 ALTER TABLE covid MODIFY id bigint(20);
 ALTER TABLE covid ADD FOREIGN KEY(hospital_id) REFERENCES hospital(id);
 
-SELECT covid.id, hospital.name FROM covid
-JOIN programmer ON covid.programmer_id = programmer.id
-JOIN hospital ON covid.hospital_id = hospital.id;
+SELECT covid.id, hospital.name
+  FROM covid
+    JOIN programmer ON covid.programmer_id = programmer.id
+    JOIN hospital ON covid.hospital_id = hospital.id;
 -- 0.0056 sec
